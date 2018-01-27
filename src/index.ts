686569afe6a5032b0e0c829735be341a354e2d6e
Oliver Joseph Ash <oliverjash@gmail.com>
2018-01-27 13:15:31 +0000

    Copy from fjbs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,63 @@
-console.log(1);
+// Copied from https://github.com/facebook/fbjs/blob/7da8335b78d669cba263760872f0a45ed16b4d12/packages/fbjs/src/core/shallowEqual.js#L39
+
+// tslint:disable-next-line no-unbound-method
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+/**
+ * inlined Object.is polyfill to avoid requiring consumers ship their own
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+ */
+// tslint:disable-next-line no-any
+function is(x: any, y: any): boolean {
+    // SameValue algorithm
+    if (x === y) {
+        // Steps 1-5, 7-10
+        // Steps 6.b-6.e: +0 != -0
+        // Added the nonzero y check to make Flow happy, but it is redundant
+        // return x !== 0 || y !== 0 || 1 / (x as number) === 1 / (y as number);
+        return x !== 0 || y !== 0 || 1 / x === 1 / y;
+    } else {
+        // Step 6.a: NaN == NaN
+        return x !== x && y !== y;
+    }
+}
+
+export function shallowEqual<A, B>(objA: A, objB: B): boolean {
+    if (is(objA, objB)) {
+        return true;
+    }
+
+    if (
+        typeof objA !== 'object' ||
+        objA === null ||
+        typeof objB !== 'object' ||
+        objB === null
+    ) {
+        return false;
+    }
+
+    type AKey = keyof typeof objA;
+    type BKey = keyof typeof objB;
+
+    // tslint:disable-next-line no-any
+    const keysA = (Object.keys(objA) as any) as AKey[];
+    // tslint:disable-next-line no-any
+    const keysB = (Object.keys(objB) as any) as BKey[];
+
+    if (keysA.length !== keysB.length) {
+        return false;
+    }
+
+    // Test for A's keys different from B.
+    for (let i = 0; i < keysA.length; i++) {
+        if (
+            !hasOwnProperty.call(objB, keysA[i]) ||
+            // tslint:disable-next-line no-any
+            !is(objA[keysA[i]], ((objB as any) as A)[keysA[i]])
+        ) {
+            return false;
+        }
+    }
+
+    return true;
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,8 +1,75 @@
+// Copied from https://github.com/facebook/fbjs/blob/7da8335b78d669cba263760872f0a45ed16b4d12/packages/fbjs/src/core/__tests__/shallowEqual-test.js
+
 import test = require('tape');
 
-test('foo', t => {
-    console.log('bar');
-    t.equal(1, 1);
+import { shallowEqual } from '../src/index';
+
+test('returns false if either argument is null', t => {
+    t.equal(shallowEqual(null, {}), false);
+    t.equal(shallowEqual({}, null), false);
+
+    t.end();
+});
+
+test('returns true if both arguments are null or undefined', t => {
+    t.equal(shallowEqual(null, null), true);
+    t.equal(shallowEqual(undefined, undefined), true);
+
+    t.end();
+});
+
+test('returns true if arguments are not objects and are equal', t => {
+    t.equal(shallowEqual(1, 1), true);
+
+    t.end();
+});
+
+test('returns true if arguments are shallow equal', t => {
+    t.equal(shallowEqual({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 }), true);
+
+    t.end();
+});
+
+test('returns true when comparing NaN', t => {
+    t.equal(shallowEqual(NaN, NaN), true);
+
+    t.equal(
+        shallowEqual(
+            { a: 1, b: 2, c: 3, d: NaN },
+            { a: 1, b: 2, c: 3, d: NaN },
+        ),
+        true,
+    );
+
+    t.end();
+});
+
+test('returns false if arguments are not objects and not equal', t => {
+    t.equal(shallowEqual(1, 2), false);
+
+    t.end();
+});
+
+test('returns false if only one argument is not an object', t => {
+    t.equal(shallowEqual(1, {}), false);
+
+    t.end();
+});
+
+test('returns false if first argument has too many keys', t => {
+    t.equal(shallowEqual({ a: 1, b: 2, c: 3 }, { a: 1, b: 2 }), false);
+
+    t.end();
+});
+
+test('returns false if second argument has too many keys', t => {
+    t.equal(shallowEqual({ a: 1, b: 2 }, { a: 1, b: 2, c: 3 }), false);
+
+    t.end();
+});
+
+test('returns false if arguments are not shallow equal', t => {
+    t.equal(shallowEqual({ a: 1, b: 2, c: {} }, { a: 1, b: 2, c: {} }), false);
 
     t.end();
 });
